241c29942e93101378eeeffacb4f0e8b82404759
glinares92 <gerson.linares@abank.com.sv>
2021-12-16 00:53:08 -0600

    Se realizo limpieza en el archivo enviroment de PRD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -32,18 +32,4 @@ export const environment = {
     tokeprov: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNlZ3Vyb3Mvc2lzYSIsImlzcyI6ImF1dGgwIiwiaWF0IjoxNTUwODY1NzE4fQ.qCOjyzMr2Urcd6Qw0mHsevlP8pent8b5n5PyYxxFVzg'
   },
 
-
-  // sisa: {
-  //   conectar: '/api/v1/conectar',
-  //   getDepartamento: '/api/v1/departamentos',
-  //   getTipoproveedor: '/api/v1/tipoproveedor',
-  //   getEspecialidada: '/api/v1/especialidades',
-  //   getBuscar: '/api/v1/proveedor/buscar',
-  //   getProveedor: '/api/v1/proveedor/sucursal',
-  //   serviceToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6IkFzZWd1cmF0ZTAxQ29uczFzYSIsImlzcyI6InNpc2EwMSJ9.AyB1IFILiXn1DHtQ0_SVwMY2qK9OHU1Q402UwREv09Y',
-  //   tokeprov: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNlZ3Vyb3Mvc2lzYSIsImlzcyI6ImF1dGgwIiwiaWF0IjoxNTUwODY1NzE4fQ.qCOjyzMr2Urcd6Qw0mHsevlP8pent8b5n5PyYxxFVzg'
-  // },
-
-
-
 };
