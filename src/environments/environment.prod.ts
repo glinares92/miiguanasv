483973a1b446d1800b75587f23486e9e85f97cf5
PORTILLO RAMOS HENRY ALBERTO <2527952016@mail.utec.edu.sv>
2021-12-15 23:35:14 -0600

    Habilitar servicio de departamentos del proyecto miiguanasv de angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -78,7 +78,7 @@ export const environment = {
     getDepartamento: 'https://www.sisa.com.sv/SISAServiceRest/api/v1/departamentos',
     getTipoproveedor: 'https://www.sisa.com.sv/SISAServiceRest/api/v1/tipoproveedor',
     getEspecialidada: 'https://www.sisa.com.sv/SISAServiceRest/api/v1/especialidades',
-    getBuscar: 'https://www.sisa.com.sv/SISAServiceRest/api/v1/proveedor/buscar',
+    getBuscar: 'https://www.sisa.com.sv/SISAServiceRest/api/v1/proveedor/busca',
     getProveedor: 'https://www.sisa.com.sv/SISAServiceRest/api/v1/proveedor/sucursal',
     serviceToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6IkFzZWd1cmF0ZTAxQ29uczFzYSIsImlzcyI6InNpc2EwMSJ9.AyB1IFILiXn1DHtQ0_SVwMY2qK9OHU1Q402UwREv09Y',
     tokeprov: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNlZ3Vyb3Mvc2lzYSIsImlzcyI6ImF1dGgwIiwiaWF0IjoxNTUwODY1NzE4fQ.qCOjyzMr2Urcd6Qw0mHsevlP8pent8b5n5PyYxxFVzg'
